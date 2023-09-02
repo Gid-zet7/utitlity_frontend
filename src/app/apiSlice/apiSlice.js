81aca2f198d070a881e7b9639053c98d85fbfd0c
Gideon Adonteng <gadonteng002@st.ug.edu.gh>
2023-09-02 19:14:59 +0000

    Updates

--- src/app/apiSlice/apiSlice.js
+++ src/app/apiSlice/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://localhost:2023",
+  baseUrl: "https://utility-bill.onrender.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
